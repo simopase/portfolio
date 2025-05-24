de4593f8d13d6c7bf01672cf2ba079370fb38230
simopase <simopase@hotmail.it>
2025-05-24 18:30:43 +0200

    feat: add framer-motion for enhanced animations; update LenisScrollProvider to utilize frame updates

--- src/app/providers/lenis-provider.tsx
+++ src/app/providers/lenis-provider.tsx
@@ -1,14 +1,28 @@
 // lenis-provider.tsx
 "use client";
 import { ReactLenis } from "lenis/react";
-import { FC, useRef } from "react";
+import type { LenisRef } from 'lenis/react';
+
+import { FC, useRef, useEffect} from "react";
+import { cancelFrame, frame } from 'framer-motion';
+
 
 type LenisScrollProviderProps = {
   children: React.ReactNode;
 };
 const LenisScrollProvider: FC<LenisScrollProviderProps> = ({ children }) => {
-  const lenisRef = useRef(null);
-  
+  const lenisRef = useRef<LenisRef>(null)
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp
+      lenisRef.current?.lenis?.raf(time)
+    }
+
+    console.log(frame)
+    frame.update(update, true)
+
+    return () => cancelFrame(update)
+  }, [])
   return <ReactLenis ref={lenisRef} root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>{children}</ReactLenis>;
 };
 
